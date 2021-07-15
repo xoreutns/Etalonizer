277e2f69ff1f3bec0b37347ca6d2675e23c7ec8a
xoreutns <xoreutns@gmail.com>
2021-07-15 11:59:25 +0300

    analyze button now scans selected dir and shows info about found files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets
 from user_interface import Ui_MainWindow  # импорт нашего сгенерированного файла
 import sys
+import os
 
 
 class MyWindow(QtWidgets.QMainWindow):
@@ -9,10 +10,28 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.btn_select_folder.clicked.connect(self.select_folder)
+        self.ui.btn_analyze.clicked.connect(self.analyze_files)
 
     def select_folder(self):
         folder_name = QtWidgets.QFileDialog.getExistingDirectory(self, 'Выберите папку', '.')
         self.ui.le_folder_name.setText(folder_name)
+        self.ui.btn_analyze.setEnabled(True)
+
+    def analyze_files(self):
+        self.ui.tb_information.clear()
+        path = self.ui.le_folder_name.text()
+        with os.scandir(path) as files:
+            for file in files:
+                if file.name.endswith('.csv') and file.is_file():
+                    self.ui.tb_information.insertPlainText('Найден файл "' + file.name + '" ')
+                    self.ui.tb_information.insertPlainText('Обработка... ')
+
+
+
+                    self.ui.tb_information.insertPlainText('ОК' + '\n')
+            if self.ui.tb_information.toPlainText() == '':
+                self.ui.tb_information.insertPlainText('Файлы *.csv не найдены. Выберите другую папку')
+
 
 
 
@@ -21,4 +40,3 @@ application = MyWindow()
 application.show()
 
 sys.exit(app.exec())
-
